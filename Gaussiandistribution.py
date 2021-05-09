8cbbe178bdb6f6a348a1a846f63cc38e0c8bc372
Edivaldo Junior <joseedivaldocg@hotmailcom>
2021-05-09 01:56:04 -0300

    feat: add calc. and visu. to Gaussian distribution class
    The formulas and visualizatoin were added. Formulas: mean, stdev,
    guassian. Vizualization: histogram and sccater plot to be a
    
    Commit_ID: 002.1

--- Gaussiandistribution.py
+++ Gaussiandistribution.py
@@ -13,12 +13,35 @@ class Guassian(Distribution):
         Distribution.__init__(self, mu, sigma)
 
     def calculate_mean(self):
+        """
+        """
 
-        return
+        avg = 1.0 * sum(self.data) / len(self.data)
+
+        self.name = avg
+
+        return avg
 
     def calculate_stdev(self, sample=True):
+        """
+        """
+        if sample:
+            n = len() - 1
+        else:
+            n = len()
 
-        return
+        mean = self.calculate_mean()
+
+        sigma = sum([math.pow(d - mean, 2) for d in self.data])
+
+        sigma = math.sqrt(sigma/n)
+        
+        self.stdev = sigma 
+
+        return self.stdev
+
+
+
     def read_data_file(self, file_name, sample=True):
         """
         function to read in the txt file. The txt file have to have 
@@ -56,18 +79,54 @@ class Guassian(Distribution):
         """
         """
 
+        plt.hist(self.data)
+        plt.title('Histogram of Data')
+        plt.xlabel('data')
+        plt.ylabel('count')
 
     
     def pdf(self, x):
         """
         """
 
-        return
+        return (1.0 / self.stdev * math.sqrt(2*math.pi)) * math.exp(-0.5*((x-self.mean)/self.stdev)**2)
     
     def plot_histogram_pdf(self, n_spaces=50):
 
         """
         """
+        mu = self.mean
+        sigma = self.stdev 
 
-        return
+        min_range = min()
+        max_range = max()
 
+        # calculates the interval between x values
+        interval = 1.0 * (max_range - min_range) / n_spaces
+
+        x = []
+        y = []
+
+        for i in range(n_spaces):
+            tmp = min_range + interval * i
+            x.append(tmp)
+            y.append(self.pdf(tmp))
+
+
+        fig, axes = plt.subplot(2, sharex=True)
+        fig.subplots_adjust(hspace = .5)
+        # histogram plot
+        axes[0].his(self.data, density=True)
+        axes[0].set_title('Normed Hitogram of Data')
+        axes[0].set_ylabel()
+
+        # scatter plot
+        axes[1].plot(x, y)
+        axes[1].set_title('Normal Distribution for \n Sample Mean and Sample Standard Deviation')
+
+        plt.show()
+
+        return x, y
+
+    
+    
