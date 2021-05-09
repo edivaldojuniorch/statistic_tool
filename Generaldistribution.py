46776357e71c1e25e0bc382636a8f46d2ea36eae
Edivaldo Junior <joseedivaldocg@hotmailcom>
2021-05-09 01:01:51 -0300

    docs: adding docs to general distribution class
    the general distribution gives the possibility to build a heritance
    architecture on the statistical_tool package. Adding the docs, it
    will helps develops to understand better the whole class.
    
    Commit_ID: 001.1

--- Generaldistribution.py
+++ Generaldistribution.py
@@ -1,14 +1,31 @@
 class Distribution:
     def __init__(self, mu=0, sigma=1):
         """
-        
+        Generic distribution class for initialize and read data set 
+        to be analysed.
+
+        Attributes:
+            mean (float) represente the mean value of the distribution
+            stdev (float) represents the standard deviaion of the distribution
+            data_list (lis of floats) a list of floats extrected from the data file
+
         """
         self.mean = mu
         self.stdev = sigma
         self.data = []
+
     
     def read_data_file(self, file_name):
         """
+        function to read in the txt file. The txt file have to have 
+        one number (float) per line. The exctracted numbers are 
+        stored in the data attribute.
+
+        Asgs:
+            file_name(string): name of the a file to read from.
+        
+        Returns:
+            none
 
         """
         # open document file
