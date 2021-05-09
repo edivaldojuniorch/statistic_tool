0d6a0998081ff46391a681f3073e7676082620ac
Edivaldo Junior <joseedivaldocg@hotmailcom>
2021-05-09 03:50:29 -0300

    feat: add calc. and visu. to Binomial distribution class
    The formulas and visualizatoin were added. 'replace_stats_with_data'
    methos was added to treat discrete caracterist from the data Binomial
    distribution.
    
    Commit_ID: 003.1

--- Binomialdistribution.py
+++ Binomialdistribution.py
@@ -15,45 +15,76 @@ class Binomial(Distribution):
         n (int): number of trials
 
     """
-    def __init__(self, mu=0, sigma=1):
+    def __init__(self, prob=0.5, size=20):
         
-        Distribution.__init__(self,mu, sigma)
+        Distribution.__init__(self, self.calculate_mean(), self.calculate_stdev())
 
         pass
 
     def calculate_mean(self):
         """
         """
-        
-        pass
+
+        self.mean = self.p * self.n
+
+        return self.mean
     
     def calculate_stdev(self):
         """
         """
+        self.stdev = math.sqrt(self.n * self.p * (1-self.p))
 
-        pass
+        return self.stdev
 
     def replace_stats_with_data(self):
         """
         """
-        
-        pass
+        self.n = len(self.data)
+        self.p = 1.0 * sum (self.data) / len(self.data)
+        self.mean = self.calculate_mean()
+        self.stdev = self.calculate_stdev()
+
+        return self.p, self.n
+
 
     def plot_bar(self):
         """
         """
+
+        plt.bar( x = [], height= [(1 -self.p) *self.n, self.p * self.n])
+        plt.title('Bar chart of Data')
+        plt.xlabel('outcome')
+        plt.ylabel('count')
         
         pass
 
     def pdf(self):
         """
         """
+
+        a = math.factorial(self.n) / (math.factorial(k) * (math.factorial(self.n - k)))
+        b = (self.p ** k )
         
-        pass
+        return a * b
 
     def plot_bar_pdf(self):
         """
         """
+        x = []
+        y = []
+
+        for i in range():
+            x.append()
+            y.append()
         
-        pass
+
+        # make the plots
+        plt.bar(x,y)
+        plt.title('Distribution of Outcomes')
+        plt.ylabel('Probability')
+        plt.xlabel('Outcome')
+        
+        plt.show()
+
+        return x, y
 
